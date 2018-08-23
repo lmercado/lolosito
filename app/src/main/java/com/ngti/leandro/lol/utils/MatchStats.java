0e7aba9e1209ece0f1dcd6080abc1d2e8576108f
leandromercado <leandro@ngti.nl>
2018-08-23 07:52:54 +0200

    kda calc and small change

--- app/src/main/java/com/ngti/leandro/lol/utils/MatchStats.java
+++ app/src/main/java/com/ngti/leandro/lol/utils/MatchStats.java
@@ -1,10 +1,17 @@
 package com.ngti.leandro.lol.utils;
 
+import java.text.DecimalFormat;
+
 public class MatchStats {
 
     public static String calculateMatchKda(int kills, int deaths, int assists) {
-        float kda;
-        kda = (kills + (1 / 3 * assists) - deaths);
-        return String.valueOf(kda);
+        double dKills = kills;
+        double dDeaths = deaths;
+        double dAssists = assists;
+        double kda;
+        DecimalFormat df = new DecimalFormat("#.##");
+        kda = (dKills + dAssists) / Math.max(1, dDeaths);
+        String formatted = df.format(kda);
+        return String.valueOf(formatted);
     }
 }
